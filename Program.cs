7934625629654c4a8887023ead6d154b4c44b922
Sebastian Macias Jimenez <sebastian.macias@globant.com>
2026-09-25 16:51:04 -0500

    feat: agregar Swagger/OpenAPI para documentacion interactiva

--- Program.cs
+++ Program.cs
@@ -3,11 +3,25 @@ using ApiProductos.Filters;
 using ApiProductos.Middleware;
 using ApiProductos.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ApiProductos",
+        Version = "v1",
+        Description = "API REST para administrar productos y categorías",
+        Contact = new OpenApiContact
+        {
+            Name = "Sebastian Macias",
+            Email = "sebastian.macias@globant.com"
+        }
+    });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=productos.db"));
@@ -16,10 +30,12 @@ builder.Services.AddScoped<IProductoService, ProductoService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.MapOpenApi();
-}
+    options.SwaggerEndpoint("/openapi/v1.json", "ApiProductos v1");
+    options.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
